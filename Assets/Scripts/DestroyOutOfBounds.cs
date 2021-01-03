6795c9e03262e7c0f05d10d251aac0050fef8cc5
josse995 <jomore03@ucm.es>
2021-01-03 15:39:31 +0100

    Initial commit
    Initial commit of Unity Learn > Programming Basics > Unit 2

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBound = 40;
+    public float lowerBound = -10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z >= topBound)
+            Destroy(gameObject);
+        else if(transform.position.z < lowerBound)
+        {
+            Debug.Log("Game over");
+            Destroy(gameObject);
+        }
+    }
+}
